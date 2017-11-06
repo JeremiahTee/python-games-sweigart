e44c637fa2de79d8205d72388cdbbec5c5a09f27
Jeremiah <jeremiahteee@gmail.com>
2017-11-05 21:32:04 -0400

    wrote rules for 'pico' 'fermi' 'bagels'
    to-do:
    -write core function

--- bagels.py
+++ bagels.py
@@ -14,3 +14,17 @@ def getSecretNum():
 
 def getClues(guess, secretNum):
     #Returns a string with the Pico, Fermi, & Bagels clues to the user.
+    if guess == secretNum:
+        return 'You got it!'
+
+    clues = []
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+        if len(clues) == 0:
+            return 'Bagels'
+
+    clues.sort()
+    return ' '.join(clues)

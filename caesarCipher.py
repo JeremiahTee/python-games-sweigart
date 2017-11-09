dea8962dbbff7bf85173a0a263c8f4a2fbfc1f6a
Jeremiah <jeremiahteee@gmail.com>
2017-11-09 15:15:43 -0500

    initial commit, declared variables & commenting

---
+++ caesarCipher.py
@@ -0,0 +1,5 @@
+# Caesar Cypher, make use of string method find()
+# include uppercase and lower case symbols
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+MAX_KEY_SIZE = len(SYMBOLS)
+

4ca76bf0cf20030987ee452d96d30c7581ae085c
Jeremiah <jeremiahteee@gmail.com>
2017-11-09 15:30:16 -0500

    getTranslatedMessage() and comments
    function incomplete

--- caesarCipher.py
+++ caesarCipher.py
@@ -15,3 +15,24 @@ def getMode():
 def getMsg():
     print('Enter your message:')
     input()
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter the key number (1-%s' % (MAX_KEY_SIZE))
+        key = int(input())
+        if(key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key #for decrypting, we shift to the left
+    translated = ''
+
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol) #returns -1 if symbol not found
+        if symbolIndex == -1: #Symbol not found in SYMBOLS
+            # add the symbol without any change
+            translated += symbol
+        else:
+            # Encrypt or decrypt

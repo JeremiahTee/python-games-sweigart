73292c113bf3807b2e24ba3dd07f40c19f13d198
Jeremiah <jeremiahteee@gmail.com>
2017-11-09 15:19:11 -0500

    wrote def getMode() and getMsg()

--- caesarCipher.py
+++ caesarCipher.py
@@ -3,3 +3,15 @@
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 MAX_KEY_SIZE = len(SYMBOLS)
 
+def getMode():
+    while True:
+        print('Do you wish to encrypt or decrypt a message?')
+        mode = input().lower()
+        if mode in ['encrypt', 'decrypt', 'e', 'd']:
+            return mode
+        else:
+            print('Enter either "encrypt" or "decrypt" or \'e or \'d')
+
+def getMsg():
+    print('Enter your message:')
+    input()

8e6fca1654d3d64c00f3eeb2cb19ac87662e9517
Jeremiah <jeremiahteee@gmail.com>
2017-11-10 19:33:15 -0400

    wrote functions drawBoard(), getNewBoard(), isValidMove()
    missing implementation of isOnBoard() to verify whether user's move input is actually on the board

--- reversegam.py
+++ reversegam.py
@@ -8,6 +8,26 @@ def drawBoard(board):
     print('     12345678')
     print('     +--------+')
     for y in range(HEIGHT):
-        print('%s|' % (y+1), end='') 
+        print('%s|' % (y+1), end='') # left column
         for x in range(WIDTH):
             print(board[x][y], end='')
+        print('%s|' % (y + 1), end='') # right column
+    print('     +--------+')
+    print('     12345678')
+
+def getNewBoard():
+    # Create a brand-new, blank board data structure
+    board = []
+    for i in range (WIDTH):
+        board.append([' ',' ',' ',' ',' ',' ',' ',' '])
+    return board
+
+def isValidMove(board, tile, xstart, ystart):
+    # Return False if the player's move on space xstart, ystart is invalid
+    # If it is a valid move, return a list of spaces that would become the player's if they move here
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'

cf094b7f5f5e3e273d1d8a1af57eac6782d15d0c
Jeremiah <jeremiahteee@gmail.com>
2017-11-13 17:04:40 -0400

    set up text, added white bg, added green polygon

--- pygameHelloWorld.py
+++ pygameHelloWorld.py
@@ -17,3 +17,17 @@ BLUE = (0, 0, 255)
 
 # Set up the fonts
 basicFont = pygame.font.SysFont(None, 48)
+
+# Set up the text
+text = basicFont.render('Hello world!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+# Draw the white background onto the surface
+windowSurface.fill(WHITE)
+
+# Draw a green polygon onto the surface.
+pygame.draw.line(windowSurface, BLUE, (60,60), (120,60), 4)
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60,120))
+pygame.draw.line(windowSurface, BLUE, (60,120), (120,120), 4)

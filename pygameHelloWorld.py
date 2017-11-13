7f9bc9272d3ca9da82135a5170ca3e69ee6eb34a
Jeremiah <jeremiahteee@gmail.com>
2017-11-13 17:01:40 -0400

    added colors & basic font

--- pygameHelloWorld.py
+++ pygameHelloWorld.py
@@ -7,3 +7,13 @@ pygame.init()
 # Set up the window
 windowSurface = pygame.display.set_mode((500,400), 0, 32)
 pygame.display.set_caption('Hello world')
+
+# Set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255 ,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Set up the fonts
+basicFont = pygame.font.SysFont(None, 48)

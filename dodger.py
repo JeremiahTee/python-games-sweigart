e7f666f522f27c4d3f5296e3619bf8d711297933
Jeremiah <jeremiahteee@gmail.com>
2017-11-15 13:53:54 -0500

    setup pygame, sound, images, "Start" screen

--- dodger.py
+++ dodger.py
@@ -37,3 +37,31 @@ def drawText(text, font, surface, x, y):
     textObj = font.render(text, 1, TEXT_COLOR)
     textRect = textObj.get_rect()
     surface.blit(textObj, textRect)
+
+# Set up pygame, the window, and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Dodger')
+pygame.mouse.set_visible(False)
+
+# Set up the game fonts
+font = pygame.font.SysFont("comicsansms", 48)
+
+# Set up sounds
+gameOverSound = pygame.mixer.Sound('gameover.wav')
+pygame.mixer.music.load('background.mid')
+
+# Set up images
+playerImage = pygame.image.load('player.png')
+playerRect = playerImage.get_rect()
+baddieImage = pygame.image.load('baddie.png')
+
+# Show the "Start" screen.
+windowSurface.fill(BACKGROUND_COLOR)
+drawText('Dodger', font, windowSurface, (WINDOW_WIDTH / 3), (WINDOW_HEIGHT / 3))
+drawText('Press a key to start', font, windowSurface, (WINDOW_WIDTH / 3) - 30, (WINDOW_HEIGHT / 3) + 50)
+pygame.display.update()
+waitForPlayerToPressKey()
+
+topScore = 0
